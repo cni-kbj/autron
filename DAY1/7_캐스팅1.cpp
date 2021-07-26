061716b45ec3f15e4ab33c9ad79923cc02b82bd9
ecourse <smkang@codenuri.co.kr>
2021-07-26 15:47:01 +0900

    Update 7_캐스팅1.cpp

--- DAY1/7_캐스팅1.cpp
+++ DAY1/7_캐스팅1.cpp
@@ -1,4 +1,4 @@
-﻿// 10_캐스팅1.cpp    48 page
+// 10_캐스팅1.cpp    48 page
 #include <iostream>
 
 // C 언어의 캐스팅은 "너무나 위험" 합니다.
@@ -20,14 +20,22 @@ int main()
 
 int main()
 {
-	const int c = 10;
+//	const int c = 10;
+	int n = 10;
+	const int c = n;
 
-	int* p = &c;
+//	int* p = &c; // error. 상수의 주소를
+				 // int* 에 담을수 없다.(const int* 는 담을수 있습니다.)
+
+	int* p = (int*)&c; // ok.. 이것이 허용됩니다.(말도 안되는 캐스팅인데)
 
 	*p = 20;
 	
-	std::cout << *p << std::endl;
-	std::cout << c << std::endl;
+	// 아래 결과 예측해 보세요.
+	std::cout << *p << std::endl;// 20
+	std::cout << c << std::endl; // 이 코드가 얼마인지는 "표준에 정의 되어 있지 않습니다."
+								 // 그래서, 컴파일러마다 다를수 있는데..
+								 // 대부분 10입니다.
 }
 
 

7fbdbaab634fd6f61e59fa91b2550402b9eb1ae4
ecourse <smkang@codenuri.co.kr>
2021-07-26 11:21:29 +0900

    Add files via upload

---
+++ DAY1/2_입출력1.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstdio>
+#include <iostream> 
+
+int main()
+{
+	int n = 10;
+
+	// C style 입출력
+	printf("n = %d\n", n); // C style
+	scanf("%d", &n);	   // 입력
+
+	// C++ style
+	std::cout << "n = " << n << std::endl;  // std::endl 은 "/n" 의 의미!
+
+	std::cin >> n;  // 1. &n 이 아니라 n 입니다.
+					// 2. ">>" 방향에 주의 하세요.
+}

d9102a4e74e15cd14b5576ce96dfb6073d8151b5
ecourse <smkang@codenuri.co.kr>
2021-07-26 10:27:37 +0900

    Add files via upload

---
+++ DAY1/1_namespace1.cpp
@@ -0,0 +1,24 @@
+﻿// 1_namespace.cpp - 교재 7 page
+#include <stdio.h>
+// namespace : 관련된 요소를 묶어서 관리할수 있고, 이름 충돌도 막을수 있다.
+namespace Audio
+{
+	// Audio 관련, 구조체/함수/전역변수들...
+	void init() { printf("Audio init\n"); }
+}
+
+namespace Video
+{
+	void init() { printf("Video init\n"); }
+}
+
+// global namesapce => 특정 이름공간에 포함되지 않은것!
+void init() { printf("global init\n"); }
+
+int main()
+{
+	init(); // global
+	Audio::init();
+	Video::init();
+}
+

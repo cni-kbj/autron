4cc30288e2db1cc92e71f158a18249d56d089735
ecourse <smkang@codenuri.co.kr>
2021-07-26 14:42:18 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+//#define Add(x, y) ((x) + (y) ) // 타입에 독립적일수 있지만 
+								// Add(++n, ++n) 이렇게 하면.. 완전한 버그..
+								// 매크로는 너무 위험 합니다. 되도록 금지.!!
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+double Add(double a, double b)
+{
+	return a + b;
+}
+
+int main()
+{
+	std::cout << Add(1,   2 ) << std::endl;
+	std::cout << Add(1.1, 2.2) << std::endl;
+}
+

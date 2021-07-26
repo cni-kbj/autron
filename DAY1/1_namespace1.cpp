6bc258983ed64e3b2dd7523d0f1007f4509bca78
ecourse <smkang@codenuri.co.kr>
2021-07-26 10:28:35 +0900

    Update 1_namespace1.cpp

--- DAY1/1_namespace1.cpp
+++ DAY1/1_namespace1.cpp
@@ -1,4 +1,4 @@
-﻿// 1_namespace.cpp - 교재 7 page
+// 1_namespace.cpp - 교재 7 page
 #include <stdio.h>
 // namespace : 관련된 요소를 묶어서 관리할수 있고, 이름 충돌도 막을수 있다.
 namespace Audio
@@ -10,6 +10,14 @@ namespace Audio
 namespace Video
 {
 	void init() { printf("Video init\n"); }
+	
+	// 중첩될수 있습니다.
+	namespace 2D
+	{
+	}
+	namespace 3D
+	{
+	}
 }
 
 // global namesapce => 특정 이름공간에 포함되지 않은것!
@@ -22,3 +30,4 @@ int main()
 	Video::init();
 }
 
+// github.com/codenuri/autron/ 에 실시간으로 소스 올리 겠습니다.

80ec5a5a143ae51320b83e6425698a342f80ffcf
ecourse <smkang@codenuri.co.kr>
2021-07-26 14:35:49 +0900

    Add files via upload

---
+++ DAY1/4_함수6.cpp
@@ -0,0 +1,20 @@
+﻿// 4_함수6. 후위 반환 타입
+
+/*
+int square(int a)
+{
+	return a * a;
+}
+*/
+
+// C++11 에서 등장하는 새로운 함수 모양
+// 후위 반환 타입(suffix return type) 이라는 문법
+auto square(int a) -> int
+{
+	return a * a;
+}
+
+int main()
+{
+	square(3);
+}

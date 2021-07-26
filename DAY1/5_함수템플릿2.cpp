16942f9c36669b04120577eb25aad8c596b01dfe
ecourse <smkang@codenuri.co.kr>
2021-07-26 14:46:30 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+
+// 구현이 유사한 함수가 타입별로 필요 하면 
+// 함수를 만들지 말고 "틀"을 만들자
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	// 정확한 표기법
+	std::cout << Add<int>(1, 2)        << std::endl;
+	std::cout << Add<double>(1.1, 2.2) << std::endl;
+
+	// 타입 인자 생략가능합니다.
+	std::cout << Add(1, 2) << std::endl;
+	std::cout << Add(1.1, 2.2) << std::endl;
+
+	// 그런데... 아래처럼 하면 어떻게 될까요 ?
+	std::cout << Add<double>(1, 2.2) << std::endl; // 문제 없습니다. T는 무조건 double
+	std::cout << Add(1, 2.2) << std::endl; /// ?????? error
+}
+

01c81233ba132d2488a28001bb4a1139768a8d40
ecourse <smkang@codenuri.co.kr>
2021-07-26 15:30:51 +0900

    Update 6_반복문.cpp

--- DAY1/6_반복문.cpp
+++ DAY1/6_반복문.cpp
@@ -1,4 +1,4 @@
-﻿// 8_반복문 38 page
+// 8_반복문 38 page
 #include <iostream>
 
 int main()
@@ -12,8 +12,8 @@ int main()
 	// C++ 부터 새로운 for 문 도입됩니다.
 	// range -for 라고 합니다. - C#, java 의 foreach, python : "for e in x" 와 유사
 	//for (int n : x) // x 자리는 "배열", STL 의 list, vector 등 모두 가능
-	for (auto n : x)
-	{
+	for (auto n : x)  // x 자리에 사용자가 만든 것(list등)을 넣으려면 
+	{				  // begin()/end()라는 약속된 이름의 함수가 필요 합니다. - 마지막날
 		std::cout << n << std::endl;
 	}
 

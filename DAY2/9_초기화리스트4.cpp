76089e09a94f96d1d5e5c69835d41daf5e411a5d
ecourse <smkang@codenuri.co.kr>
2021-07-27 15:32:22 +0900

    Add files via upload

---
+++ DAY2/9_초기화리스트4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+// 선언과 구현의 분리
+class Point
+{
+	int x, y;
+public:
+	Point(int a = 0, int b = 0);
+};
+
+// 멤버 함수의 외부 구현은 "반환타입 클래스이름::함수이름()" 입니다.
+// => 초기화 리스트는 구현에 적어야 합니다.
+// => 함수 인자의 디폴트 값은 함수 선언에만 있어야 합니다.
+Point::Point(int a, int b) 
+	: x(a), 
+	  y(b)
+{
+
+}
+
+
+int main()
+{
+
+}
+
+
+
+

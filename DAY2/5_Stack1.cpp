4cd326b11dfa3013019ecc5ca9ab2855ae3f3fbf
ecourse <smkang@codenuri.co.kr>
2021-07-27 11:04:45 +0900

    Add files via upload

---
+++ DAY2/5_Stack1.cpp
@@ -0,0 +1,23 @@
+﻿// 2_Stak1 - 60 page ~
+#include <iostream>
+// 스택을 만들어 봅시다.
+// 버전 1. C언어
+// 단점 : 만약 "stack"이 2개이상 필요하다면 ?
+int buff[10];
+int idx = 0;
+
+void push(int n)
+{
+	buff[idx++] = n;
+}
+int pop()
+{
+	return buff[ --idx ];
+}
+
+int main()
+{
+	push(10);
+	push(20);
+	std::cout << pop() << std::endl; // 20
+}

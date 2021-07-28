9af008fa449de145c91c1c57eb7a1a91ff473017
ecourse <smkang@codenuri.co.kr>
2021-07-28 11:47:40 +0900

    Add files via upload

---
+++ DAY3/4_상속1.cpp
@@ -0,0 +1,23 @@
+﻿// 5_상속1.cpp   132page ~
+#include <iostream>
+#include <string>
+
+class Student 
+{				
+	std::string name;
+	int    age;
+	int    id;
+};
+
+class Professor
+{
+	std::string name;
+	int    age;
+	int    major;
+};
+
+int main()
+{
+	Student s;
+	Professor p;
+}
